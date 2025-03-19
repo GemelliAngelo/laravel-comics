e2bb524b38937e5a1fc71bb9c460652c24132c20
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-19 22:54:37 +0100

    fix .env + first components

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="d-flex">
+        <div class="row row-cols-3">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+    <div class="d-flex"></div>
+</footer>

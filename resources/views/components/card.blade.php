51e3261fb900311c239cf073d7a2137f97533cba
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-19 23:25:45 +0100

    add card components + slots

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <img src="{{$cover}}" class="card-img-top" alt="cover">
+    <div class="card-body">
+      <h5 class="card-title">{{$slot}}</h5>
+    </div>
+  </div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,4 +2,14 @@
 
 @section("content")
 <h1>Hello world</h1>
+
+<div class="row">
+    <div class="col">
+        <x-card>
+            <x-slot:cover>#</x-slot:cover>
+            Titolo
+        </x-card>
+    </div>
+</div>
+
 @endsection

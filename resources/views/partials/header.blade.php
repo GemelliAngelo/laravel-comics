dd0f92dcf5acd478470159f6497aa31144bbf291
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-20 00:04:10 +0100

    header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,8 +4,19 @@
         <nav class="navbar bg-body-tertiary">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="DC" width="30" height="24">
+                    <img src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="DC">
                 </a>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item">CHARACTERS</li>
+                    <li class="list-group-item">COMICS</li>
+                    <li class="list-group-item">MOVIES</li>
+                    <li class="list-group-item">TV</li>
+                    <li class="list-group-item">GAMES</li>
+                    <li class="list-group-item">COLLECTIBLES</li>
+                    <li class="list-group-item">VIDEOS</li>
+                    <li class="list-group-item">FANS</li>
+                    <li class="list-group-item">SHOP</li>
+                  </ul>
             </div>
         </nav>
     </div>

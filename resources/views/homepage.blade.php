03158e8700ae16bfce78ade5293e1e4ce464498b
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-20 00:10:09 +0100

    add config

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,23 @@
 @extends("layouts.master")
 
 @section("content")
-<div class="container">
-    <div class="row">
+
+@php
+    $comics= config("comics")
+@endphp
+
+<div class="container py-5 my-3">
+    <div class="row row-cols-6 g-5">
+        
+        @foreach ($comics as $comic)
+            
         <div class="col">
             <x-card>
                 <x-slot:cover>#</x-slot:cover>
                 Titolo
             </x-card>
         </div>
+        @endforeach
     </div>
 </div>
 

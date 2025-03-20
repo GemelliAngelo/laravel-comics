333f4c5f452d9816a353fa941ff1bdcc9b3ecfca
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-20 15:03:31 +0100

    add footer debug

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,15 @@
 <footer>
-    <div class="container">
-
-        <div class="d-flex">
+    
+    <div class="footer-body">
+            <div class="container">
             <div class="row row-cols-3">
                 <div class="col"></div>
                 <div class="col"></div>
                 <div class="col"></div>
             </div>
+            </div>
         </div>
-        <div class="d-flex"></div>
-    </div>
+        <div class="footer-footer">
+            <div class="container"></div>
+        </div>
 </footer>

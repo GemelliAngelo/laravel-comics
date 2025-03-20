890251b044197effcf828b1e2e2c9c0c20120687
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-20 16:37:37 +0100

    finish comics page front end

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,94 @@
 <footer>
-    
     <div class="footer-body">
-            <div class="container">
-            <div class="row row-cols-3">
-                <div class="col"></div>
-                <div class="col"></div>
-                <div class="col"></div>
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-5 my-auto">
+                    <div class="row g-0">
+
+                        <div class="col">
+                            <div>
+                                <h5>DC COMICS</h5>
+                                <ul class="footer-list">
+                                    <li>Characters</li>
+                                    <li>Comics</li>
+                                    <li>Movies</li>
+                                    <li>TV</li>
+                                    <li>Games</li>
+                                    <li>Videos</li>
+                                    <li>News</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h5>SHOP</h5>
+                                <ul class="footer-list">
+                                    <li>Shop DC</li>
+                                    <li>Shop DC Collectibles</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <h5>DC</h5>
+                            <ul class="footer-list">
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h5>SITES</h5>
+                            <ul class="footer-list">
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="logo col-7">
+                </div>
             </div>
+                
             </div>
         </div>
-        <div class="footer-footer">
-            <div class="container"></div>
+    <div class="footer-footer">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-4">
+                <button type="button" class="btn btn-lg btn-outline-primary text-white">SIGN UP NOW</button>
+                <div class="d-flex align-items-center gap-4">
+                    <div>
+                        <span class="fs-4 text-primary">
+                            FOLLOW US
+                        </span>
+                    </div>
+                    <span>
+                        <img src="{{Vite::asset("resources/img/footer-facebook.png")}}" alt="facebook">
+                    </span>
+                    <span>
+                        <img src="{{Vite::asset("resources/img/footer-twitter.png")}}" alt="facebook">
+                    </span>
+                    <span>
+                        <img src="{{Vite::asset("resources/img/footer-youtube.png")}}" alt="facebook">
+                    </span>
+                    <span>
+                        <img src="{{Vite::asset("resources/img/footer-pinterest.png")}}" alt="facebook">
+                    </span>
+                    <span>
+                        <img src="{{Vite::asset("resources/img/footer-periscope.png")}}" alt="facebook">
+                    </span>
+                </div>
+            </div>
         </div>
+    </div>
 </footer>
